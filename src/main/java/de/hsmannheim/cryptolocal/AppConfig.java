94fa37817365f0eef116cc413f94d6312d668070
fab-du <batlab.official@gmail.com>
2016-01-23 04:49:43 +0100

    minor

--- src/main/java/de/hsmannheim/cryptolocal/AppConfig.java
+++ src/main/java/de/hsmannheim/cryptolocal/AppConfig.java
@@ -12,7 +12,6 @@ public class AppConfig extends WebMvcConfigurerAdapter {
 
 	@Override
 	public void addInterceptors( InterceptorRegistry registry ){
-		//registry.addInterceptor(new LocaleChangeInterceptor());
 		registry.addInterceptor( new AuthInterceptor());	
 	}
 }
