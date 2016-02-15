81dfc7dcb7f62c311ee28aad9b3e6826eb860537
fab-du <batlab.official@gmail.com>
2016-02-15 11:34:13 +0100

    update security

--- src/main/java/de/hsmannheim/security/SecurityConfig.java
+++ src/main/java/de/hsmannheim/security/SecurityConfig.java
@@ -1,8 +1,11 @@
 package de.hsmannheim.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,6 +15,9 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Order(1)
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
+	@Autowired
+	AuthenticationManager authManager;
+	
 	public SecurityConfig() {
 		super(true);
 	}
@@ -19,13 +25,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	@Override 
 	protected void configure(HttpSecurity http) throws Exception {
 	    http
-	        .csrf()
-	            .disable()
+        .csrf()
+        .disable();
+	    
+	    http
+	    	.exceptionHandling().and()
+	    	.anonymous().and()
+	    	.servletApi().and()
+	    	.headers().cacheControl();
+	    
+	    http
 	        .authorizeRequests()
 	            .antMatchers("/session/**").permitAll()
 	            .anyRequest().authenticated();
 	}
-	
+
+	@Bean
 	@Override
 	public AuthenticationManager authenticationManagerBean() throws Exception {
 	    return super.authenticationManagerBean();
