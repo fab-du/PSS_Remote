a8a90b526d70c1840aa90f904b2972ca96bc9674
fab-du <batlab.official@gmail.com>
2016-02-17 11:29:48 +0100

    minor changes

--- src/main/java/de/hsmannheim/cryptolocal/Application.java
+++ src/main/java/de/hsmannheim/cryptolocal/Application.java
@@ -1,19 +1,35 @@
 package de.hsmannheim.cryptolocal;
 import java.io.File;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication
 @EnableScheduling
+@ImportResource({ "classpath:security.xml" })
 @EnableAutoConfiguration
 public class Application extends SpringBootServletInitializer{
 
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		//servletContext.getSessionCookieConfig().
+		//servletContext.addFilter(filterName, filterClass)
+		servletContext.setInitParameter("filter", "");
+		super.onStartup(servletContext);
+	}
+	
+	
+	
+	
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(Application.class);
